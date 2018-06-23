137d023dee09d1c8db88f815eb7b06c06569c018
goatpig <moothecowlord@gmail.com>
2018-06-23 14:08:50 +0200

    add missing file

---
+++ cppForSwig/BDVCodec.h
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  Copyright (C) 2018, goatpig.                                              //
+//  Distributed under the MIT license                                         //
+//  See LICENSE-MIT or https://opensource.org/licenses/MIT                    //                                      
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _H_BDV_CODEC_
+#define _H_BDV_CODEC_
+
+#include "protobuf/compiled/AddressBook.pb.h"
+#include "protobuf/compiled/AddressData.pb.h"
+#include "protobuf/compiled/CommonTypes.pb.h"
+#include "protobuf/compiled/FeeEstimate.pb.h"
+#include "protobuf/compiled/LedgerEntry.pb.h"
+#include "protobuf/compiled/Utxo.pb.h"
+#include "protobuf/compiled/NodeStatus.pb.h"
+#include "protobuf/compiled/BDVCommand.pb.h"
+
+#endif
