271091336dc3b6250475765ad6cae647b066d467
goatpig <moothecowlord@gmail.com>
2019-05-16 19:12:55 +0200

    fix queue reader thread wake up

--- cppForSwig/ThreadSafeClasses.h
+++ cppForSwig/ThreadSafeClasses.h
@@ -406,11 +406,8 @@ private:
 		}
 
       std::unique_lock<std::mutex> lock(condVarMutex_);
-		if (flag_ > 0)
-		{
-			--flag_;
-			return;
-		}
+      if (flag_ > 0)
+         return;
 
 		condVar_.wait(lock);
    }
@@ -482,9 +479,8 @@ public:
 		{
          std::unique_lock<std::mutex> lock(condVarMutex_);
 			++flag_;
-		}
-
-		condVar_.notify_one();
+   		condVar_.notify_all();
+      }
    }
 
    void terminate(std::exception_ptr exceptptr = nullptr)
