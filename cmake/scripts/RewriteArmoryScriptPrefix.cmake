9ee4ea66726d8f55db350039dafe616d3513b29d
Rafael Kitover <rkitover@gmail.com>
2019-07-05 19:33:48 +0000

    cmake: minor fixes
    Fix CMakeSettings.json for prior vcpkg related changes so that GUI
    builds will work.
    
    Fix the `armory` prefix editing script to not leave behind a temp
    directory.
    
    Remove changes to bundled libwebsockets and instead override the cmake
    cache variable in the place where the subdirectory is added.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/scripts/RewriteArmoryScriptPrefix.cmake
+++ cmake/scripts/RewriteArmoryScriptPrefix.cmake
@@ -4,12 +4,12 @@ string(REGEX REPLACE " /usr" " ${prefix}" script_edited ${script_contents})
 
 file(MAKE_DIRECTORY ${script_dir}/script_tmp)
 
-file(WRITE ${script_dir}/script_tmp/${script_output_file} ${script_edited})
+file(WRITE ${script_dir}/script_tmp/${script_name} ${script_edited})
 
 file(
-    COPY ${script_dir}/script_tmp/${script_output_file}
+    COPY ${script_dir}/script_tmp/${script_name}
     DESTINATION ${script_dir}
     FILE_PERMISSIONS OWNER_WRITE OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 )
 
-file(REMOVE ${script_dir}/script_tmp)
+file(REMOVE_RECURSE ${script_dir}/script_tmp)
