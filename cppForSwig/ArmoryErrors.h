a754ba41e048d26c3a23b6cd912b13207b7d28e3
goatpig <moothecowlord@gmail.com>
2020-02-12 15:20:29 +0100

    update error code enum

--- cppForSwig/ArmoryErrors.h
+++ cppForSwig/ArmoryErrors.h
@@ -13,13 +13,15 @@ enum class ArmoryErrorCodes : int
 {
     //bitcoin node rpc
     ZcBroadcast_AlreadyInChain = -27,
+    ZcBroadcast_Error          = -25, //non specific error, most likely spent output
 
     //common
     ErrorUnknown    = -1,
     Success         = 0,
 
     //p2p reject errors
-    P2PReject_InsufficientFee = 66,
+    P2PReject_Duplicate         = 18, //mempool double spend
+    P2PReject_InsufficientFee   = 66,
 
     //zc parser
     ZcBatch_Timeout                 = 30000,
