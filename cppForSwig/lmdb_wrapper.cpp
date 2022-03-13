5da73fc618e8362610379e4d246a3aa9e370f731
goatpig <moothecowlord@gmail.com>
2022-03-13 11:54:51 +0100

    increase size limits for supernode tables

--- cppForSwig/lmdb_wrapper.cpp
+++ cppForSwig/lmdb_wrapper.cpp
@@ -39,17 +39,17 @@ using namespace Armory::Config;
 
 const set<DB_SELECT> LMDBBlockDatabase::supernodeDBs_({});
 const map<string, size_t> LMDBBlockDatabase::mapSizes_ = {
-   {"headers", 4 * 1024 * 1024 * 1024ULL},
-   {"blkdata", 1024 * 1024ULL},
-   {"history", 1024 * 1024ULL},
+   {"headers", 50 * 1024 * 1024 * 1024ULL},
+   {"blkdata", 50 * 1024 * 1024ULL},
+   {"history", 50 * 1024 * 1024ULL},
    {"txhints", 20 * 1024 * 1024 * 1024ULL},
-   {"ssh",   500 * 1024 * 1024 * 1024ULL},
+   {"ssh",   2000 * 1024 * 1024 * 1024ULL},
    {"subssh", 2000 * 1024 * 1024 * 1024ULL},
-   {"subssh_meta", 100 * 1024 * 1024ULL},
+   {"subssh_meta", 1024 * 1024 * 1024ULL},
    {"stxo", 2000 * 1024 * 1024 * 1024ULL},
    {"zeroconf", 10 * 1024 * 1024 * 1024ULL},
-   {"txfilters", 10 * 1024 * 1024 * 1024ULL},
-   {"spentness", 500 * 1024 * 1024 * 1024ULL},
+   {"txfilters", 100 * 1024 * 1024 * 1024ULL},
+   {"spentness", 2000 * 1024 * 1024 * 1024ULL},
 };
 
 ////////////////////////////////////////////////////////////////////////////////
